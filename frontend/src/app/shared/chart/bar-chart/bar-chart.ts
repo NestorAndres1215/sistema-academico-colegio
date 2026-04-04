945d1795c6bb356a214b9b804f56b23d9d11fc24
NestorAndres1215 <atiroandres@gmail.com>
2026-04-03 23:13:00 -0500

    subiendo

--- frontend/src/app/shared/chart/bar-chart/bar-chart.ts
+++ frontend/src/app/shared/chart/bar-chart/bar-chart.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild, OnChanges } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-bar-chart',
@@ -7,5 +10,52 @@ import { Component } from '@angular/core';
   styleUrl: './bar-chart.css',
 })
 export class BarChart {
+
+  @Input() labels: string[] = [];
+  @Input() data: number[] = [];
+  @Input() titulo: string = 'Gráfico de Barras';
+  @ViewChild('barCanvas') barCanvas!: ElementRef<HTMLCanvasElement>;
+  barChart!: Chart;
+
+
+  ngAfterViewInit(): void {
+    if (this.labels.length > 0 && this.data.length > 0) {
+      this.renderChart();
+    }
+  }
+
+  ngOnChanges(): void {
+    if (this.barChart) {
+      this.barChart.destroy();
+    }
+    if (this.labels.length > 0) {
+      this.renderChart();
+    }
+  }
+
+  renderChart() {
+    console.log(this.labels)
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            label: 'Porcentaje por Módulo (%)',
+            data: this.data,
+            backgroundColor: 'rgba(54, 162, 235, 0.6)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: { beginAtZero: true }
+        }
+      }
+    });
+  }
 
 }
--- frontend/src/app/shared/chart/pie-chart/pie-chart.ts
+++ frontend/src/app/shared/chart/pie-chart/pie-chart.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit, OnChanges } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-pie-chart',
@@ -7,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrl: './pie-chart.css',
 })
 export class PieChart {
+ @Input() titulo: string = 'Grafico de Porcentaje';
+  @Input() labels: string[] = [];
+  @Input() data: number[] = [];
 
+  @ViewChild('pieCanvas') pieCanvas!: ElementRef;
+
+  chart!: Chart;
+
+  ngAfterViewInit() {
+    this.renderChart();
+  }
+
+  ngOnChanges() {
+    if (this.chart) {
+      this.chart.destroy();
+      this.renderChart();
+    }
+  }
+
+  renderChart() {
+
+    if (!this.pieCanvas) return;
+
+    this.chart = new Chart(this.pieCanvas.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: this.labels,
+        datasets: [
+          {
+            data: this.data, // ← NÚMEROS
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (context: any) => {
+                const label = context.label || '';
+                const value = context.raw; 
+                return ` ${value}%`; 
+              }
+            }
+          }
+        }
+      }
+    });
+  }
 }
