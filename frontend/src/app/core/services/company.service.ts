7d6f91e205e5f86ac525768ad1178743391bf6fd
NestorAndres1215 <atiroandres@gmail.com>
2026-04-08 11:36:27 -0500

    se arreglo una parte de los routes y la parte de company service

--- frontend/src/app/core/services/company.service.ts
+++ frontend/src/app/core/services/company.service.ts
@@ -16,4 +16,20 @@ export class CompanyService {
     return this.http.get<any>(`${this.backendUrl}/company`);
   }
 
+  getById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.backendUrl}/company/${id}`);
+  }
+
+  getByName(name: string): Observable<any> {
+    return this.http.get<any>(`${this.backendUrl}/company/name/${name}`);
+  }
+
+  create(data: FormData, id: String) {
+    return this.http.post(`${this.backendUrl}/company/`, data);
+  }
+
+  update(id: string, data: FormData) {
+    return this.http.put(`${this.backendUrl}/company/${id}`, data);
+  }
+
 }
--- frontend/src/app/features/routes/admin.routes.ts
+++ frontend/src/app/features/routes/admin.routes.ts
@@ -2,16 +2,22 @@ import { Routes } from "@angular/router";
 
 export const ADMIN_ROUTES: Routes = [
 
-        {
+    {
         path: 'admin',
         loadComponent: () => import('../admin/admin-main/admin-main')
             .then(m => m.AdminMain)
     },
-
     {
         path: 'usuarios',
         loadChildren: () =>
             import('../admin/admin-user.routes')
                 .then(m => m.ADMIN_USER_ROUTES)
     },
+    {
+        path: 'profesores',
+        loadChildren: () =>
+            import('../admin/admin-teacher.routes')
+                .then(m => m.ADMIN_TEACHER_ROUTES)
+    }
+    
 ];
